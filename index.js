58cc71eb306b81eacb5ac8439388f4da171f4ab8
Shimon Mondol <shimonuap072@gmail.com>
2025-01-28 22:51:37 +0600

    delete-added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const port = 5000;
+const app = express();
 //localhost:5000/add
 
+app.use(cors());
+app.use(express.json());
+
+// Connect Database
 mongoose
   .connect(
     "mongodb+srv://Crud:1234@cluster0.x7kff.mongodb.net/Crud?retryWrites=true&w=majority&appName=Cluster0"
@@ -12,24 +15,41 @@ mongoose
   .then(() => {
     console.log("Database is Connected");
   });
-app.use(cors());
-app.use(express.json());
 
-const User = mongoose.model("User", { task: String });
-
-app.post("/add", (req, res) => {
-  // let { name } = req.body;
-  // console.log(name);
-  // res.send(name);
-  let { name } = req.body;
-
-  let result = new User({
-    task: name,
-  });
-  result.save();
-  res.send(result);
+const todo = mongoose.model("Todo", {
+  name: String,
+  age: Number,
+  email: String,
 });
 
-app.listen(port || 5000, () => {
-  console.log(`Example app listening on port ${port}`);
+// Add Task
+app.post("/create", (req, res) => {
+  console.log(req.body.name);
+
+  const result = new todo({
+    name: req.body.name,
+  });
+
+  result.save();
+  res.status(201).send({ success: true, msg: "todo created successful" });
+});
+// All Todos
+app.get("/alltodos", async (req, res) => {
+  let alltodos = await todo.find({});
+  res.send(alltodos);
+});
+
+// delete toto
+app.delete("/deletetodo/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletetodo = await todo.findOneAndDelete({
+    _id: id,
+  });
+});
+// res
+//   .status(200)
+//   .send({ success: true, msg: "deleted successful", data: deletetodo });
+
+app.listen(3000, () => {
+  console.log("Server is running");
 });
